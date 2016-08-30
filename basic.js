f6d705858fd4801a62444a8e3c490eab2049ed64
ronghuaxueleng <caoqianghappy@126.com>
2016-08-30 17:08:30 +0800

    修改

--- basic.js
+++ basic.js
@@ -1,30 +1,3 @@
-function demoFromHTML() {
-    var pdf = new jsPDF('p', 'pt', 'letter')
-    var source = $('#fromHTMLtestdiv').get(0);
-    var specialElementHandlers = {
-        '#bypassme': function(element, renderer){
-            return true
-        }
-    };
-
-    var margins = {
-      top: 0,
-      bottom: 60,
-      left: 0,
-      width: 522
-    };
-    pdf.fromHTML(source, margins.left, margins.top, {
-            'width': margins.width,
-            'elementHandlers': specialElementHandlers
-        },
-        function (dispose) {
-          pdf.save('Test.pdf');
-        },
-        margins
-    )
-}
-
-
 $(function(){
     $('#downloadPng').click(function(){
         div2png($("#fromHTMLtestdiv"),'png')
